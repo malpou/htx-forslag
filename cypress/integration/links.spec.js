aea2412865eb4c6bc8902b2b310beebcce9e2bf8
Malthe Poulsen <malpoudev@gmail.com>
2021-12-03 17:11:11 +0100

    Add cypress

---
+++ cypress/integration/links.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+
+describe('Frontpage', () => {
+    it('login btn should navigate to the login page', () => {
+        // Start from the index page
+        cy.visit('/')
+    
+        // Find a link with an href attribute containing "about" and click it
+        cy.get("#btn-login").click()
+    
+        // The new url should include "/enter"
+        cy.url().should('include', '/enter')
+    })
+
+    it('logo should navigate back to home pagpe', () => {
+        // Start from the index page
+        cy.visit('/enter')
+
+        // Find logo and click it
+        cy.get(".btn-logo").click()
+
+        // The new url should include "/"
+        cy.url().should('include', '/')
+    })
+})
